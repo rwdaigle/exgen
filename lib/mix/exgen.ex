5f4b6b93140eae2b3a1b77182b2da83360a7d1a9
Cees de Groot <casedeg@gmail.com>
2018-07-20 14:21:19 -0400

    Bump Elixir/OTP version to current

--- lib/mix/exgen.ex
+++ lib/mix/exgen.ex
@@ -13,7 +13,7 @@ defmodule Mix.Exgen do
   end
 
   def in_tmp(function) do
-    subdir = :crypto.rand_bytes(10) |> Base.hex_encode32
+    subdir = :crypto.strong_rand_bytes(10) |> Base.hex_encode32
     tmp_path = Path.join(System.tmp_dir!, subdir)
     File.mkdir_p! tmp_path
     File.cd! tmp_path, function
